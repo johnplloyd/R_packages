67e4d5a1516545d419b3e33c8c12117e82168235
“johnplloyd” <lloydjp@med.umich.edu>
2018-05-03 12:01:27 -0400

    fixed median and quantile calc bug

--- ordered_dot_plot.R
+++ ordered_dot_plot.R
@@ -14,10 +14,23 @@ remove_outliers <- function (x){
 
 ordered_dot_plot <- function (x, ylim='', xlab=NA, ylab=NA, spread=0.3, iqr_line=TRUE, median_line=TRUE, outlier=TRUE){
   
+  # if lacking, add names to x
   if( is.null (names (x))){
     names(x) <- 1:length (x)
   }
   
+  # calculate and store median and quantile values
+  # calculated prior to removal of outliers, if requested
+  if(median_line == TRUE){
+    medn_l <- lapply(x, median)
+  }
+  
+  if(iqr_line == TRUE){
+    quant_l <- lapply(x, quantile)
+  }
+  
+  # remove outliers
+  # resulting vectors may not be of equal length: return as list of vectors
   if(outlier == FALSE){
     x.rm <- list()
     i = 1
@@ -31,17 +44,24 @@ ordered_dot_plot <- function (x, ylim='', xlab=NA, ylab=NA, spread=0.3, iqr_line
     x <- x.rm
   }
   
+  # calculate y limits as min and max of all data
   if( ylim == '' ){
     y_min <- min (unlist (x))
     y_max <- max (unlist (x))
     ylim <- c (y_min, y_max)
   }
   
+  # generate empty plot of appropriate dimensions
+  # chart is effectively a scatterplot with custom x-axis
   plot (1, type = "n", xlim = c (1-spread, length(x)+spread), ylim = ylim, xaxt = 'n', xlab=xlab, ylab=ylab)
   
+  # loop through x and plot data
+  # y values: data distribution, sorted low to high
+  # x values: even spread across subset of x axis
   for (i in 1:length (names (x))){
     print(i)
     nm <- names (x)[i]
+    
     y_vals <- sort(x[[nm]])
     
     spread_min <- i-spread
@@ -49,8 +69,8 @@ ordered_dot_plot <- function (x, ylim='', xlab=NA, ylab=NA, spread=0.3, iqr_line
     x_vals <- seq (from=spread_min, to = spread_max, length=length(y_vals))
     
     if (iqr_line == TRUE){
-      iqr_low <- quantile (y_vals)[[2]]
-      iqr_high <- quantile (y_vals)[[4]]
+      iqr_low <- quant_l[[i]][[2]]
+      iqr_high <- quant_l[[i]][[4]]
       
       segments (x0=spread_min, y0=iqr_low, x1=spread_max, y1=iqr_low, col = "gray40", lty="dotted")
       segments (x0=spread_min, y0=iqr_high, x1=spread_max, y1=iqr_high, col = "gray40", lty="dotted")
@@ -59,11 +79,11 @@ ordered_dot_plot <- function (x, ylim='', xlab=NA, ylab=NA, spread=0.3, iqr_line
     points (x=x_vals, y=y_vals, pch=16, cex=0.6)
     
     if (median_line == TRUE){
-      medn <- median (y_vals)
-      
+      medn <- medn_l [[i]]
       segments (x0=spread_min, y0=medn, x1=spread_max, y1=medn, col = "red", lwd=2)
     }
   }
   
+  # add custom x axis
   axis (1, at=1:length (x), labels=names (x))
 }
