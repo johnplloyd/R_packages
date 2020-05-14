d6e649cd136d156f70d8d977548b45cefcfae576
John Lloyd <john.paul.lloyd@gmail.com>
2020-05-14 18:02:13 -0400

    update ML scripts and corner

---
+++ lloydML/R/train_and_apply_under_CV.R
@@ -0,0 +1,62 @@
+#' Train and apply machine learning algorithms under cross-validation
+#'
+#' This function will train a prediction model and apply it to new data.
+#' @param model_type algorithm to use: "glment", "plsr"
+#' @param y response vector - training
+#' @param X feature matrix - training
+#' @param newy response vector - testing (plsr only)
+#' @param newx feature matrix - testing
+#' @param parameters_obj object with algorithm-specific parameters
+#' @return Returns an object with three items: 1) vector with predicted response values for the testing instances ($predicted), 2) reporter items, such as parameters selected ($report_items), and 3) model object ($model)
+#' @export
+
+train_and_apply_under_CV <- function( model_type, Y, X, n_fold, n_rep, parameters_obj ){
+
+  y <- Y[,1]
+  x <- as.matrix(X)
+  n_instances <- length(y)
+
+  CV_folds_obj <- lapply(X = 1:n_rep, FUN = function(i) assign_CV_folds(n_instances = n_instances, n_fold = n_fold) )
+
+  df_pred <- data.frame( row.names = rownames(X) )
+  report_obj <- list()
+  for( i in 1:n_rep ){
+    print(c(i, "=== === === ==="))
+    CV_folds_vec <- CV_folds_obj[[i]]
+    CV_folds_vec
+
+    pred_vec <- c()
+    report_obj1 <- list()
+    for(j in 1:n_fold){
+      print(c("        ", j))
+      testing_ind <- which(CV_folds_vec == j)
+      training_ind <- which(CV_folds_vec != j)
+
+      y.train <- y[training_ind]
+      y.test <- y[testing_ind]
+
+      X.train <- x[training_ind, , drop = F]
+      X.test <- x[testing_ind, , drop = F]
+
+      modelObj <- train_and_apply_model(model_type = model_type, y = y.train, X = X.train, newy = y.test, newx = X.test, parameters_obj = parameters_obj )
+
+      pred_vec <- c(pred_vec, modelObj$predicted)
+      report_obj1[[j]] <- modelObj$report_items
+    }
+
+    df_pred <- cbind(df_pred, pred_vec[row.names(df_pred)] )
+    names(df_pred)[i] <- paste("rep", i, sep = "")
+
+    names(report_obj1) <- paste("fold", 1:n_fold, sep = "")
+
+    report_obj[[i]] <- report_obj1
+    names(report_obj)[i] <- paste("rep", i, sep = "")
+  }
+
+  pred_mean <- rowMeans(df_pred)
+  pred_var <- apply( X = df_pred, MARGIN = 1, FUN = var )
+
+  returnObj <- list(df_pred = df_pred, report_obj = report_obj, CV_folds = CV_folds_obj, pred_mean = pred_mean, pred_var = pred_var )
+
+  return( returnObj )
+}
--- lloydUtils/R/corner.R
+++ lloydUtils/R/corner.R
@@ -1,13 +1,33 @@
-#' Prints the top left corner of a matrix
+#' Prints the corner of a matrix
 #'
-#' This function prints the topleft corner of a matrix.
+#' This function prints a corner of a matrix. Which corner can be assigned with the 'c' parameter: "topleft"
 #' @param X matrix or data.frame
 #' @param n num of columns and rows to print
-#' @return Prints the topleft corner of a matrix
+#' @param c matrix corner to print: "topleft" (default), "topright", "bottomleft", and "bottomright"
+#' @return Prints the corner of a matrix
 #' @export
 #' @examples
-#' corner(X = matrix)
+#' X <- matrix(data = 1:80, nrow = 10)
+#'
+#' corner(X = X, n = 2, c = "topleft")
+#' >     [,1] [,2]
+#' >[1,]    1   11
+#' >[2,]    2   12
+#'
+#' corner(X = X, n = 3, c = "bottomright")
+#' >      [,1] [,2] [,3]
+#' > [1,]   58   68   78
+#' > [2,]   59   69   79
+#' > [3,]   60   70   80
 
-corner <- function(X, n = 10){
-  print(m[1:n, 1:n])
+corner <- function(X, n = 6, c = "topleft"){
+  if(tolower(c) == "topleft" | tolower(c) == "tl"){
+    print(X[1:n, 1:n])
+  }else if(tolower(c) == "topright" | tolower(c) == "tr"){
+    print(X[1:n, (ncol(X)-n+1):ncol(X)])
+  }else if(tolower(c) == "bottomleft" | tolower(c) == "bl"){
+    print(X[(nrow(X)-n+1):nrow(X), 1:n])
+  }else if(tolower(c) == "bottomright" | tolower(c) == "br"){
+    print(X[(nrow(X)-n+1):nrow(X), (ncol(X)-n+1):ncol(X)])
+  }
 }
