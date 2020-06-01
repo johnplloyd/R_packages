174de724db061525def82fba5bfc0ab4ebbab999
John Lloyd <john.paul.lloyd@gmail.com>
2020-06-01 10:43:03 -0400

    summary

--- lloydML/R/train_and_apply_with_validation_set.R
+++ lloydML/R/train_and_apply_with_validation_set.R
@@ -1,24 +1,17 @@
-#' Train and apply machine learning algorithms under cross-validation
+#' Train and apply machine learning algorithms with a validation set
 #'
-#' This function will hold out data by CV fold, train prediction model, and apply it to the held out fold. Acts a wrapper around train_and_apply_model().
-#' @param model_type algorithm to use: "glment", "plsr"
-#' @param y response vector
-#' @param X feature matrix
-#' @param n_fold number of CV folds: Integer
+#' This function will randomly select a subset of the training data, train a prediction model, and apply it to the validation set. Acts a wrapper around train_and_apply_model().
+#' @param model_type algorithm to use: "glmnet", "plsr"
+#' @param y response vector: training set
+#' @param X feature matrix: training set
+#' @param X.val feature matrix: valiation set
 #' @param n_rep number of repetitions: Integer
+#' @param prop_per_rep proportion of training set to randomly select per repetition: [0, 1]
 #' @param parameters_obj object with algorithm-specific parameters
-#' @return Returns an object with 5 items: 1) data frame with predicted value from each CV repetition for each instance ($df_pred), 2) object with reporter items, such as parameters selected, from each CV repetition and CV fold ($report_obj), 3) object with the CV fold assignments for each repetition ($CV_folds), 4) mean predicted value across repetitions for each instance ($pred_mean), and 5) variance in predicted values across reptitions for each instance ($pred_var)
+#' @return Returns an object with 5 items: 1) data frame with predicted value from each bagging repetition for each instance in the validation set ($df_pred), 2) object with indices of training subset selected for each repetition ($training_inds), 3) object with reporter items, such as parameters selected, from each repetition ($report_obj), 4) mean predicted value across repetitions for validation set instances ($pred_mean), and 5) standard deviation of predicted values across reptitions for validation set instances ($pred_sd)
 #' @export
 
 train_and_apply_with_validation_set <- function( model_type, y, X, X.val, n_rep, prop_per_rep, parameters_obj ){
-
-  # model_type <- "glmnet"
-  # Y1 <- m.AUC.GDSC[1]
-  # X1 <- m.RNA.GDSC.common
-  # X2 <- m.RNA.TNBC.common
-  # parameters_obj <- glmnet_parameters_obj.LASSO
-  # n_rep <- 5
-  # prop_per_rep <- 0.25
 
   if(anyNA(y)){
     NA_ind <- which(is.na(y))
@@ -33,43 +26,28 @@ train_and_apply_with_validation_set <- function( model_type, y, X, X.val, n_rep,
   n_per_rep <- floor( n_instances * prop_per_rep )
   bagging_inds <- lapply(X = 1:n_rep, FUN = function(x) sort(sample( 1:n_instances, n_per_rep )) )
   names(bagging_inds) <- paste("rep", 1:n_rep, sep = "")
-  #random_inds
 
-  df_pred <- data.frame( row.names = rownames(X2) )
+  df_pred <- data.frame( row.names = rownames(X.val) )
   report_obj <- list()
   for(i in 1:n_rep){
-    print(c(i, "=== === ==="))
+    print(paste("REPETITION", i))
     bagging_ind <- bagging_inds[[i]]
-    y1.train <- y1[bagging_ind]
-    X1.train <- X1[bagging_ind,]
+    y.train <- y[bagging_ind]
+    X.train <- X[bagging_ind,]
 
-    # length(y1.train)
-    # dim(X1)
-    # dim(X1.train)
-    # dim(X2)
-    # corner(X1)
-
-    modelObj <- train_and_apply_model(model_type = model_type, y = y1.train, X = X1.train,
-                                      newy = NULL, newx = X2,
+    modelObj <- train_and_apply_model(model_type = model_type, y = y.train, X = X.train,
+                                      newy = NULL, newx = X.val,
                                       parameters_obj = parameters_obj )
     df_pred <- cbind(df_pred, modelObj$predicted)
     names(df_pred)[i] <- paste("rep", i, sep = "")
-    #df_pred
 
     report_obj[[i]] <- modelObj$report_items
     names(report_obj)[i] <- paste("rep", i, sep = "")
-    #report_obj
   }
 
-  #head(df_pred)
-  #report_obj
-
   pred_mean <- rowMeans(df_pred)
-  #pred_mean
+  pred_sd <- apply( X = df_pred, MARGIN = 1, FUN = sd )
 
-  pred_var <- apply( X = df_pred, MARGIN = 1, FUN = var )
-  #pred_var
-
-  returnObj <- list(df_pred = df_pred, training_inds = random_inds, report_obj = report_obj, pred_mean = pred_mean, pred_var = pred_var )
+  returnObj <- list(df_pred = df_pred, training_inds = bagging_inds, report_obj = report_obj, pred_mean = pred_mean, pred_sd = pred_sd )
   return(returnObj)
 }
---
+++ testPackage/R/testFunction.R
@@ -0,0 +1,13 @@
+#' Title of a test function
+#'
+#' This function prints two copies of the input
+#' @param x item to be printed
+#' @return Prints two copies of x
+#' @export
+#' @examples
+#' testFunction(x = Hello world!")
+
+testFunction <- function(x){
+  print(x)
+  print(x)
+}
